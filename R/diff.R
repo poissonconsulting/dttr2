582815847983723db32c85851b29a0aa24ed7a20
joethorley <joe@poissonconsulting.ca>
2020-03-16 18:41:15 -0700

    - Added dtt_diff().

--- R/diff.R
+++ R/diff.R
@@ -1,27 +1,25 @@
 #' Time Difference
 #' 
 #' Gets the time difference in secs, minutes, hours, days or weeks.
+#' Uses difftime() but floors x and y first after coercing to POSIXct 
+#' and adjusts the timezone of y to match that of x.
 #'
-#' @param x An object that can be coerced to a POSIXct using date_time.
-#' @param y An object that can be coerced to a POSIXct using date_time.
+#' @param x An object that can be coerced to a POSIXct using dtt_date_time().
+#' @param y An object that can be coerced to a POSIXct using dtt_date_time().
 #' @param units A string of the units. The possible values are 
 #' "secs", "minutes", "hours", "days" or "weeks".
+#' @param as_difftime A flag specifying whether to return a difftime vector.
 #' @return A numeric vector of the time difference.
 #' @export
-dtt_diff <- function(x, y, units = "secs") {
+dtt_diff <- function(x, y, units = "secs", as_difftime = FALSE) {
   chk_string(units)
-  chk_subset(units, c("secs", "mins", "hours", "days"))
+  chk_subset(units, c("secs", "mins", "hours", "days", "weeks"))
+  chk_flag(as_difftime)
+  
   x <- dtt_date_time(x)
   y <- dtt_date_time(y)
   y <- dtt_adjust_tz(y, tz = dtt_tz(x))
-  diff <- difftime(x, y, units = "secs")
-  diff <- as.numeric(diff)
-  if(units == "secs") return(diff)
-  diff <- diff / 60
-  if(units == "mins") return(diff)
-  diff <- diff / 60
-  if(units == "hours") return(diff)
-  diff <- diff / 24
-  if(units == "days") return(diff)
-  diff / 7
+  diff <- difftime(x, y, units = units)
+  if(as_difftime) return(diff)
+  as.numeric(diff)
 }
