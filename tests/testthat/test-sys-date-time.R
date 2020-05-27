329620d1568dfc373530ddd2625a578cf5e27f4f
Joe Thorley <joe@poissonconsulting.ca>
2020-05-27 15:21:42 -0700

    test sys date time

--- tests/testthat/test-sys-date-time.R
+++ tests/testthat/test-sys-date-time.R
@@ -1,4 +1,4 @@
 test_that("sys-time", {
   expect_is(dtt_sys_date_time(), "POSIXct")
-  expect_warning(expect_true(dtt_sys_date_time("Etc/GMT+7") < dtt_sys_date_time("Etc/GMT+8")))
+  expect_warning(expect_true(dtt_sys_date_time("Etc/GMT+12") < dtt_sys_date_time("Etc/GMT-12")))
 })
