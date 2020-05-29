1bab2528a3b57e9231a1521c717468800f0ffeb3
Joe Thorley <joe@poissonconsulting.ca>
2020-05-29 10:39:19 -0700

    just test tz

--- tests/testthat/test-sys-date-time.R
+++ tests/testthat/test-sys-date-time.R
@@ -1,6 +1,4 @@
 test_that("sys-time", {
   expect_true(chk::vld_date_time(dtt_sys_date_time()))
-  print(dtt_sys_date_time("Etc/GMT+12"))
-  print(dtt_sys_date_time("Etc/GMT-12"))
-  expect_warning(expect_true(dtt_sys_date_time("Etc/GMT+12") < dtt_sys_date_time("Etc/GMT-12")))
+  expect_identical(dtt_tz(dtt_sys_date_time("Etc/GMT+12")), "Etc/GMT+12")
 })
