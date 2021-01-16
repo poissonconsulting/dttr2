66e1678a05e9a5b3d08eae66118e2639220a6c92
Joe Thorley <joe@poissonconsulting.ca>
2021-01-15 21:14:10 -0800

    - Added `dtt_days_in_month()`.

---
+++ R/days-in-month.R
@@ -0,0 +1,14 @@
+#' Days in the Month
+#'
+#' @param x A Date or POSIXct vector.
+#' @return A integer vector of 28 to 31 indicating the days in the month.
+#' @export
+#'
+#' @examples
+#' dtt_days_in_month(as.Date(c("2000-02-11", "2001-02-01")))
+dtt_days_in_month <- function(x) {
+  month <- dtt_month(x)
+  days <- .days_in_month[month]
+  days[month == 2L & dtt_leap_year(x)] <- 29L
+  days
+}
