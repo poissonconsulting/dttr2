21f6ef078d06ffd835342d8a303caec502d8d40e
Joe Thorley <joe@poissonconsulting.ca>
2021-08-03 14:36:01 -0700

    - Added `dtt_decade()`.

---
+++ R/decade.R
@@ -0,0 +1,24 @@
+#' Decade
+#' 
+#' @inheritParams params
+#' 
+#' @return A integer vector of the decade.
+#' @export
+#'
+#' @examples
+#' dtt_decade(as.Date(c("2001-01-01", "2015-12-13")))
+dtt_decade <- function(x, ...) {
+  UseMethod("dtt_decade")
+}
+
+#' @describeIn dtt_decade Decade a Date vector
+#' @export
+dtt_decade.Date <- function(x, ...) {
+  chk_unused(...)
+  year <- dtt_year(x)
+  year <- year / 10
+  year <- floor(year)
+  year <- year * 10
+  year <- as.integer(year)
+  year
+}
---
+++ tests/testthat/test-decade.R
@@ -0,0 +1,15 @@
+test_that("decade.Date", {
+  expect_identical(dtt_decade(NA_Date_[-1]), integer(0))
+  expect_identical(dtt_decade(NA_Date_), NA_integer_)
+  expect_identical(dtt_decade(as.Date("2000-01-01")), 2000L)
+  expect_identical(dtt_decade(as.Date("2009-12-31")), 2000L)
+  expect_identical(dtt_decade(as.Date("2010-01-01")), 2010L)
+  expect_identical(dtt_decade(as.Date(c("2000-01-01", NA))), c(2000L, NA_integer_))
+  expect_identical(dtt_decade(as.Date(c("2000-01-01", NA))), c(2000L, NA_integer_))
+  expect_identical(
+    dtt_decade(as.Date(c("2001-01-01", "2015-12-13"))),
+    c(2000L, 2010L))
+  bce <- as.Date("0000-01-01")
+  expect_identical(dtt_decade(bce), 0L)
+  expect_identical(dtt_decade(bce-1L), -10L)
+})
