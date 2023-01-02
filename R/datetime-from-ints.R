5344c195986a1a2b39aa95173904fbce321e3bcb
Ayla Pearson <ayla@poissonconsulting.ca>
2023-01-02 15:07:55 -0800

    write datetime from ints function and get it working

--- R/datetime-from-ints.R
+++ R/datetime-from-ints.R
@@ -39,18 +39,24 @@
 dtt_datetime_from_ints <- function(year = 1970L , month = 1L, day = 1L, 
                                    hour = 0L, minute = 0L, second = 0L, 
                                    tz = dtt_default_tz()) {
-  chk::chk_whole_number(year)
+  chk::chk_whole_numeric(year)
   chk::chk_gte(year, 0) # ask Joe if there is a better range for this 
-  chk::chk_whole_number(month)
+  chk::chk_whole_numeric(month)
   chk::chk_range(month, range = c(1L, 12L))
-  chk::chk_whole_number(day)
+  chk::chk_whole_numeric(day)
   chk::chk_range(day, range = c(1L, 31L))
-  chk::chk_whole_number(hour)
+  chk::chk_whole_numeric(hour)
   chk::chk_range(hour, range = c(0L, 23L))
-  chk::chk_whole_number(minute)
+  chk::chk_whole_numeric(minute)
   chk::chk_range(minute, range = c(0L, 59L))
-  chk::chk_whole_number(second)
+  chk::chk_whole_numeric(second)
   chk::chk_range(second, range = c(0L, 59L))
   chk::chk_string(tz)
   
+  datetimes <- dtt_date_time(
+    dtt_date(paste(year, month, day, sep = "-")),
+    dtt_time(paste(hour, minute, second, sep = ":")),
+    tz = tz
+  )
+  datetimes
 }
