35b62b5f41f1b965c9543a10081888436cf719f9
Ayla Pearson <ayla@poissonconsulting.ca>
2023-01-02 14:52:09 -0800

    writing the create dates from integer function and getting it working

--- R/date-from-ints.R
+++ R/date-from-ints.R
@@ -23,17 +23,13 @@
 #' )
 dtt_date_from_ints <- function(year = 1970L , month = 1L, day = 1L) {
   
-  chk::chk_whole_number(year)
+  chk::chk_whole_numeric(year)
   chk::chk_gte(year, 0) # ask Joe if there is a better range for this 
-  chk::chk_whole_number(month)
+  chk::chk_whole_numeric(month)
   chk::chk_range(month, range = c(1L, 12L))
-  chk::chk_whole_number(day)
+  chk::chk_whole_numeric(day)
   chk::chk_range(day, range = c(1L, 31L))
   
-  
-  
-  
-  
-  
-  
+  dates <- dtt_date(paste(year, month, day, sep = "-"))
+  dates
 }
