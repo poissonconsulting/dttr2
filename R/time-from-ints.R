dac1e181c81dbbab50cb408bd05fffb0df88b4ae
Ayla Pearson <ayla@poissonconsulting.ca>
2023-01-02 16:29:47 -0800

    adding create time from ints function

---
+++ R/time-from-ints.R
@@ -0,0 +1,37 @@
+#' Create time from vectors 
+#'
+#' @param hour An integer of the hour between 0 and 23.The default value is hour
+#'   zero.
+#' @param minute An integer of the minute between 0 to 59. The default value is
+#'   minute zero.
+#' @param second An integer of the second between 0 to 59. The default value is
+#'   second zero.
+#'
+#' @return A floored hms vector. 
+#' @export
+#'
+#' @examples
+#' dtt_time_from_ints()
+#' 
+#' dtt_time_from_ints(
+#'  hour = 10,
+#'  minute = 15,
+#'  second = 30
+#' )
+#' 
+#' dtt_time_from_ints(
+#'  hour = c(10, 11),
+#'  minute = c(15, 15),
+#'  second = c(30, 0)
+#' )
+dtt_time_from_ints <- function(hour = 0L, minute = 0L, second = 0L) {
+  chk::chk_whole_numeric(hour)
+  chk::chk_range(hour, range = c(0L, 23L))
+  chk::chk_whole_numeric(minute)
+  chk::chk_range(minute, range = c(0L, 59L))
+  chk::chk_whole_numeric(second)
+  chk::chk_range(second, range = c(0L, 59L))
+  
+  times <- dtt_time(paste(hour, minute, second, sep = ":"))
+  times
+}
