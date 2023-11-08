d10fb20b42b015b6f1a013585e82ab0e7e573849
Ayla Pearson <ayla@poissonconsulting.ca>
2023-11-07 16:04:56 -0800

    styler

--- R/excel_to_date_time.R
+++ R/excel_to_date_time.R
@@ -23,32 +23,32 @@ dtt_excel_to_date_time <- function(x, tz = dtt_default_tz(), modern = TRUE, ...)
   chk::chk_flag(modern)
   chk::chk_numeric(x)
   chk::chk_string(tz)
-  
+
   if (length(x) == 0) {
     return(dtt_date_time(integer(0)))
-  } 
-  
+  }
+
   # get date portion
   date <- floor(x)
   origin <- if (modern) as.Date("1899-12-30") else as.Date("1904-01-01")
   date <- dtt_date(date, origin = origin)
-  
+
   # get time portion
-  hour <- x%%1
+  hour <- x %% 1
   hour <- hour * 24
-  
-  minute <- hour%%1
+
+  minute <- hour %% 1
   minute <- minute * 60
-  
-  second <- minute%%1
+
+  second <- minute %% 1
   second <- second * 60
-  
+
   hour <- as.integer(floor(hour))
   minute <- as.integer(floor(minute))
   second <- as.integer(floor(second))
-  
+
   time <- dtt_time_from_ints(hour, minute, second)
-  # combine to be date time 
+  # combine to be date time
   date_times <- paste(date, time)
   date_times <- replace(date_times, date_times == "NA NA", NA_character_)
 
