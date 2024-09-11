a89a88bd71c0f6da8f1394af42b0b3710334b650
Ayla Pearson <ayla@poissonconsulting.ca>
2024-09-11 16:53:28 -0700

    update docs as per roxygen error

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,10 @@
+#' @exportS3Method
 c.POSIXct <- function(..., recursive = FALSE) {
   dots <- list(...)
   .POSIXct(c(unlist(lapply(dots, unclass))), tz = dtt_tz(dots[[1]]))
 }
 
+#' @exportS3Method
 c.hms <- function(..., recursive = FALSE) {
   dots <- list(...)
   hms::as_hms(c(unlist(lapply(dots, unclass))))
