bd75091c61dca9a469ff163343894d9df317d292
Ayla Pearson <ayla@poissonconsulting.ca>
2024-09-13 09:46:47 -0700

    update exporting of S3 methods

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,10 @@
-#' @exportS3Method
+#' @export
 c.POSIXct <- function(..., recursive = FALSE) {
   dots <- list(...)
   .POSIXct(c(unlist(lapply(dots, unclass))), tz = dtt_tz(dots[[1]]))
 }
 
-#' @exportS3Method
+#' @export
 c.hms <- function(..., recursive = FALSE) {
   dots <- list(...)
   hms::as_hms(c(unlist(lapply(dots, unclass))))
